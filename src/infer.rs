e8407764ceddf07e64283deea2af5be9638a2205
xcodebuild <me@xuan.at>
2022-06-08 13:16:16 +0800

    fix fetch icon, fix #5

--- src/infer.rs
+++ src/infer.rs
@@ -1,16 +1,14 @@
 use std::{fs::File, path::Path};
 
-use futures::io;
-use reqwest;
-use regex::Regex;
-use sanitize_html::{sanitize_str, rules::predefined::DEFAULT};
-use tokio::task::spawn_blocking;
 use download_rs::sync_download::Download;
 use log::{info, trace, warn};
-use site_icons::Icons;
-use website_icon_extract::ImageLink;
+use regex::Regex;
+use reqwest;
+use sanitize_html::{rules::predefined::DEFAULT, sanitize_str};
+use site_icons::{IconKind, Icons};
+use tokio::task::spawn_blocking;
 
-use crate::{consts::{USER_AGENT_REQUEST, TEMP_DIR}};
+use crate::consts::{TEMP_DIR, USER_AGENT_REQUEST};
 
 pub async fn get_content(url: String) -> String {
     let content = reqwest::Client::builder()
@@ -27,7 +25,6 @@ pub async fn get_content(url: String) -> String {
     return content;
 }
 
-
 pub async fn infer_title(url: String) -> String {
     let content = get_content(url).await;
     let re = Regex::new(r"<title>(.*?)</title>").unwrap();
@@ -37,30 +34,43 @@ pub async fn infer_title(url: String) -> String {
 }
 
 pub async fn infer_icon(url: String) -> String {
-    let url_str = url.clone();
-    let icon_list = spawn_blocking(move || {ImageLink::from_website(url_str, USER_AGENT_REQUEST, 5).unwrap()}).await.unwrap();
+    let mut icons = Icons::new();
+    // scrape the icons from a url
+    icons.load_website(&(url)).await;
+    let entries = icons.entries().await;
 
-    let icon: String = if icon_list.len() > 0 {
-        let iconUrl = &icon_list[0].url;
-        iconUrl.to_string()
-    } else {
-        let mut icons = Icons::new();
-        // scrape the icons from a url
-        icons.load_website(&(url)).await;
-        let entries = icons.entries().await;
-        let iconUrl = &entries[0].url;
-        iconUrl.to_string()
+    let icon = {
+        let mut icon_str = entries[0].url.to_string();
+        let mut size = 0;
+        for entry in entries {
+            let height = if entry.info.size().is_none() {
+                0
+            } else {
+                entry.info.size().unwrap().height
+            };
+            if entry.kind == IconKind::AppIcon && (size == 0 || height > size) {
+                icon_str = entry.url.to_string();
+                size = height;
+            }
+        }
+        icon_str
     };
 
+    println!("Icon: {:?}", &icon);
+
     let icon_path = Path::new(&TEMP_DIR.as_ref()).join("icon.png");
-    let icon_path_string = icon_path.as_path().to_str();
+    let icon_path_string = icon_path.as_path().to_str().unwrap();
 
-    let download = Download::new(&icon, icon_path_string,None);
+    let mut file = std::fs::File::create(icon_path_string).unwrap();
 
-    match download.download() {
-        Ok(_) => info!("Download icon success: {:?}", icon_path_string),
-        Err(e) => info!("Download error: {}",e.to_string()),
-    }
-    return icon_path_string.unwrap().to_string();
+    spawn_blocking(move || {
+        reqwest::blocking::get(icon)
+            .unwrap()
+            .copy_to(&mut file)
+            .unwrap();
+    })
+    .await
+    .unwrap();
+
+    return icon_path_string.to_string();
 }
-
